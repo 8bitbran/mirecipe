44a574f268ce30eee745e1518b19cd42ede61d76
8bitbran <brandonmar97@gmail.com>
2020-06-10 12:34:22 -0400

    Setup migrations and database

---
+++ db/migrate/20200610163007_create_recipes.rb
@@ -0,0 +1,11 @@
+class CreateRecipes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :recipes do |t|
+      t.string :title
+      t.string :category
+      t.integer :prep_time
+      t.integer :cook_time
+      t.integer :rating
+    end 
+  end
+end
