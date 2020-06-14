00c3f1651d85411851743154d6b90584eb879edb
8bitbran <brandonmar97@gmail.com>
2020-06-14 19:11:39 -0400

    Added recipes requests

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,3 +1,20 @@
 class RecipesController < ApplicationController
+    get '/recipes/new' do 
+        erb :'/recipes/create_recipe'
+    end 
 
+    post '/recipes' do 
+        if params.any?
+            recipe = Recipe.create(params)
+            redirect "/recipes/#{recipe.id}"
+        else
+            @error = 'You must fill in all fields correctly.'
+            redirect '/recipes/new'
+        end
+    end 
+
+    get '/recipes/:id' do
+        @recipe = Recipe.find_by_id(params[:id])
+        erb :'recipes/view_recipe'
+    end
 end
