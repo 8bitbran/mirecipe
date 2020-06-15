807fffbf8c2298129f9df492efe24ec1e4a23fac
8bitbran <brandonmar97@gmail.com>
2020-06-14 20:14:31 -0400

    Added update_recipe instance method

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,14 @@
 class Recipe < ActiveRecord::Base 
     belongs_to :user
+
+    def update_recipe(params)
+        @title = params[:title]
+        @description = params[:description]
+        @category = params[:category]
+        @prep_time = params[:prep_time]
+        @cook_time = params[:cook_time]
+        @servings = params[:servings]
+        @ingredients = params[:ingredients]
+        @directions = params[:directions]
+    end 
 end 
