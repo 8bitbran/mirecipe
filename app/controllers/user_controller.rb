1b4524a76514f423d0141403e115ceda28473b6e
8bitbran <brandonmar97@gmail.com>
2020-06-16 14:51:22 -0400

    Added signup and login posts

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,9 +1,44 @@
 class UsersController < ApplicationController
     get '/signup' do
-        erb :'/users/signup'
+        if Helpers.is_logged_in?(session)
+            redirect '/'
+        else 
+            erb :'/users/signup'
+        end 
+    end 
+
+    post '/signup' do 
+        if !params.empty?
+            user = User.create(params)
+            session[:user_id] = user.id
+        else 
+            redirect '/signup'
+        end 
+
+        redirect '/myrecipes'
     end 
 
     get '/login' do
         erb :'/users/login'
+    end 
+
+    post '/login' do 
+        @user = User.find_by(:username => params[:username])
+
+        if @user && @user.authenticate(params[:password])
+			session[:user_id] = @user.id
+            redirect to '/myrecipes'
+        else
+            redirect to '/login'
+		end
+    end 
+
+    get '/logout' do 
+        if Helpers.is_logged_in?(session)
+            session.clear
+            redirect '/login'
+        else
+            redirect '/'
+        end
     end 
 end
