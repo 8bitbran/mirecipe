ce7dd1c94ac1ba68276aa52945ca0a05ed327079
8bitbran <brandonmar97@gmail.com>
2020-06-17 12:42:44 -0400

    Revised helpers

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -6,7 +6,7 @@ class Helpers
     end
 
     def self.current_user(session)
-       @user = User.find_by_id(session[:user_id])
+        @user = User.find_by_id(session[:user_id])
     end
 
     def self.is_logged_in?(session)
