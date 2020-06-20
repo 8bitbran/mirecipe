b400fc19e057611cf047f9d2fbf40a489c69dbe5
8bitbran <brandonmar97@gmail.com>
2020-06-20 17:52:35 -0400

    Added valid_username? method to check username

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -2,8 +2,12 @@ require 'sinatra/base'
 
 class Helpers 
     def self.valid_email?(email)
-        email =~ /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,63}$/
+        email =~ /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
     end
+
+    def self.valid_username?(username)
+        username =~ /\A[a-z0-9_]{4,16}\z/
+    end 
 
     def self.current_user(session)
         User.find_by(id: session[:user_id])
