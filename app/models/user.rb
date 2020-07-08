0b0e1fc42699285219bc99facbeb543d59f903ad
8bitbran <brandonmar97@gmail.com>
2020-07-08 13:10:11 -0400

    Moved User validations to User class

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ActiveRecord::Base
     validates :username, uniqueness: true
     validates :username, presence: true
     has_secure_password
+
+    def self.new_with_validations(params)
+        if valid_email?(params[:email]) && valid_username?(params[:username])
+            user = User.new(params)
+        end
+    end
+
+    def self.valid_email?(email)
+        email =~ /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+    end
+
+    def self.valid_username?(username)
+        username =~ /\A[a-z0-9_]{4,16}\z/
+    end 
 end
